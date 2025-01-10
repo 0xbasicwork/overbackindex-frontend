e357796d3b422477d52580ddd8608a1c2746026e
0xbasicwork <0xbasic@pm.me>
2025-01-10 16:31:02 +0000

    Add production configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/overbackindex',
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://45.76.10.9:3000/:path*'
+      }
+    ];
+  }
+};
 
 export default nextConfig; 
