ac67ddd06053fb6d8176e7ef3e4ee6307fae3a86
0xbasicwork <0xbasic@pm.me>
2025-01-10 18:23:53 +0000

    Fix deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api',
+        source: '/',
         destination: 'http://45.76.10.9:3000/',
-        basePath: false
+        basePath: true
       }
     ];
   }
