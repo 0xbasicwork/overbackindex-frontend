86895ca44636875fb5e201b3d53297ca55735100
0xbasicwork <0xbasic@pm.me>
2025-01-10 16:24:53 +0000

    Update build configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/overbackindex',
-  output: 'standalone',
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  }
+  output: 'standalone'
 };
 
 export default nextConfig; 
