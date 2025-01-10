1f1c2994cfe0639b2ff6bd21267ce97892e7e4d7
0xbasicwork <0xbasic@pm.me>
2025-01-10 16:11:37 +0000

    Add Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,22 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/overbackindex',
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/overbackindex/api/:path*',
-          destination: 'http://45.76.10.9:3000/api/:path*',
-        },
-        {
-          source: '/overbackindex/widget',
-          destination: 'http://45.76.10.9:3000/widget',
-        },
-      ],
-    }
-  },
   reactStrictMode: true,
-  images: {
-    domains: ['sobackitsover.xyz'],
-  },
-} 
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://45.76.10.9:3000/:path*'
+      }
+    ];
+  }
+};
+
+module.exports = nextConfig; 
