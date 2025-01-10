207a3919b4dd2e2c5e9a1d6376295efbd47dac7e
0xbasicwork <0xbasic@pm.me>
2025-01-10 18:12:25 +0000

    Fix node types and update config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
+        source: '/:path*',
         destination: 'http://45.76.10.9:3000/:path*',
         basePath: false
       }
